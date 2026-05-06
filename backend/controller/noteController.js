3675a0d4812f60384ee49d75bc7745d09b8a60ac
asinijayaweera <asinijayaweera03@gmail.com>
2026-05-06 15:22:47 +0530

    feat: update noteController - replace load/reps with content/subject

---
+++ backend/controller/noteController.js
@@ -0,0 +1,87 @@
+const NoteModel = require('../models/noteModel')
+const mongoose = require('mongoose')
+
+// get all notes
+const getNotes = async (req, res) => {
+  const notes = await NoteModel.find({}).sort({ createdAt: -1 })
+  res.status(200).json(notes)
+}
+
+// get a single note
+const getNote = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such note' })
+  }
+
+  const note = await NoteModel.findById(id)
+
+  if (!note) {
+    return res.status(404).json({ error: 'No such note' })
+  }
+
+  res.status(200).json(note)
+}
+
+// create a new note
+const createNote = async (req, res) => {
+  const { title, content, subject } = req.body
+
+  let emptyFields = []
+
+  if (!title) emptyFields.push('title')
+  if (!content) emptyFields.push('content')
+  if (!subject) emptyFields.push('subject')
+
+  if (emptyFields.length > 0) {
+    return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+  }
+
+  try {
+    const note = await NoteModel.create({ title, content, subject })
+    res.status(200).json(note)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+// delete a note
+const deleteNote = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such note' })
+  }
+
+  const note = await NoteModel.findOneAndDelete({ _id: id })
+
+  if (!note) {
+    return res.status(400).json({ error: 'No such note' })
+  }
+
+  res.status(200).json(note)
+}
+
+// update a note
+const updateNote = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such note' })
+  }
+
+  const note = await NoteModel.findOneAndUpdate(
+    { _id: id },
+    { ...req.body },
+    { new: true }
+  )
+
+  if (!note) {
+    return res.status(400).json({ error: 'No such note' })
+  }
+
+  res.status(200).json(note)
+}
+
+module.exports = { getNotes, getNote, createNote, deleteNote, updateNote }
