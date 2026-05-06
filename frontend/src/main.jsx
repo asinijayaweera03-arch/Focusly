eb197bdc18cd91c21ae58e5d2c29c7631fc36a46
asinijayaweera <asinijayaweera03@gmail.com>
2026-05-06 15:26:56 +0530

    wrap app with NotesContextProvider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextProvider } from './context/WorkoutsContext';
+import { NotesContextProvider } from './context/NotesContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
+    <NotesContextProvider>
       <App />
-    </WorkoutsContextProvider>
+    </NotesContextProvider>
   </React.StrictMode>
 )
 
